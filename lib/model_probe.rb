f62fefdfdd42f9916479c4d242820c3926fec377
Nathan Hopkins <natehop@gmail.com>
2012-10-30 17:27:05 -0600

    Moved color out and added default values to the output.

--- lib/model_probe.rb
+++ lib/model_probe.rb
@@ -1,27 +1,14 @@
 require "model_probe/version"
+require "model_probe/color"
 
 module ModelProbe
-  colors = {
-    :red     => 31,
-    :green   => 32,
-    :yellow  => 33,
-    :blue    => 34,
-    :magenta => 35,
-    :cyan    => 36,
-    :white   => 37
-  }
-
-  colors.each do |name, code|
-    define_method name do |text|
-    "\e[#{code}m#{text}\e[0m"
-    end
-  end
+  include ModelProbe::Color
 
   # Pretty prints column meta data for an ActiveModel.
   def probe
-    name_pad = columns.map{|c| c.name.length}.max + 1
-    type_pad = columns.map{|c| c.type.length}.max + 2
-    sql_type_pad = columns.map{|c| c.sql_type.length}.max + 1
+    name_pad = columns.map{ |c| c.name.length }.max + 1
+    type_pad = columns.map{ |c| c.type.length }.max + 2
+    sql_type_pad = columns.map{ |c| c.sql_type.length }.max + 1
 
     columns.sort{ |a, b| a.name <=> b.name }.map do |column|
       name = column.name
@@ -30,7 +17,8 @@ module ModelProbe
       print " "
       print blue(column.type.to_s.ljust(type_pad, "."))
       print magenta(column.sql_type.to_s.ljust(sql_type_pad))
-      print red("NULL") if column.null
+      column.null ? print(red("NULL")) : print("    ")
+      print " [#{column.default}]" if column.default
       puts
     end
     nil
@@ -43,7 +31,7 @@ module ModelProbe
 
     columns.sort{ |a, b| a.name <=> b.name }.map do |column|
       next if column.name =~ /^(created_at|updated_at)$/
-      hash[name][column.name] = "value"
+      hash[name][column.name] = column.default || "value"
     end
 
     puts hash.to_yaml
