3b7861b3278fddda7e1448151e25c4e531a8f6ec
Nathan Hopkins <natehop@gmail.com>
2016-08-12 12:28:49 -0400

    Fix formatting

--- lib/model_probe.rb
+++ lib/model_probe.rb
@@ -53,9 +53,9 @@ module ModelProbe
     puts "  # validations ..............................................................."
     validation_columns.sort_by(&:name).each do |column|
       next if column.name == primary_key
-      puts "  validates :#{column.name} presence: :true" unless column.null
+      puts "  validates :#{column.name}, presence: :true" unless column.null
       if %i(text string).include?(column.type) && column.limit.to_i > 0
-        puts "  validates :#{column.name} length: { maximum: #{column.limit} }"
+        puts "  validates :#{column.name}, length: { maximum: #{column.limit} }"
       end
     end
     puts if validation_columns.size > 0
